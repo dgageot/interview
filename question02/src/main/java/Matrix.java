7e93fcda3875e074d63e9c209eb967d1304dfd16
David Gageot <david@gageot.net>
2015-07-01 11:27:06 +0200

    Simpler code

--- question02/src/main/java/Matrix.java
+++ question02/src/main/java/Matrix.java
@@ -1,15 +1,12 @@
 public class Matrix {
-    private final int[][] values;
     private final long[][] sums;
 
     public Matrix(int[][] values) {
-        this.values = values;
         this.sums = preCalcSums(values);
     }
 
     private static long[][] preCalcSums(int[][] values) {
         long[][] sums = new long[values.length][];
-
         for (int y = 0; y < values.length; y++) {
             sums[y] = new long[values[y].length];
         }
@@ -36,13 +33,9 @@ public class Matrix {
     }
 
     public long sum(int x1, int y1, int x2, int y2) {
-        return precalc_sum(x2, y2)
-                - precalc_sum(x2, y1 - 1)
-                - precalc_sum(x1 - 1, y2)
-                + precalc_sum(x1 - 1, y1 - 1);
-    }
-
-    private long precalc_sum(int x2, int y2) {
-        return sums[y2][x2];
+        return sums[y2][x2]
+                - sums[y1 - 1][x2]
+                - sums[y2][x1 - 1]
+                + sums[y1 - 1][x1 - 1];
     }
 }
