993198d146453c03f316ea6be86c6412075989a3
David Gageot <david@gageot.net>
2015-07-01 11:07:51 +0200

    Base algo

--- question02/src/main/java/Matrix.java
+++ question02/src/main/java/Matrix.java
@@ -1,8 +1,19 @@
 public class Matrix {
+    private final int[][] values;
+
     public Matrix(int[][] values) {
+        this.values = values;
     }
 
     public long sum(int x1, int y1, int x2, int y2) {
-        return 273;
+        long sum = 0L;
+
+        for (int y = y1; y <= y2; y++) {
+            for (int x = x1; x <= x2; x++) {
+                sum += values[y][x];
+            }
+        }
+
+        return sum;
     }
 }
--- question02/src/test/java/MatrixTest.java
+++ question02/src/test/java/MatrixTest.java
@@ -17,4 +17,19 @@ public class MatrixTest {
 
         assertThat(sum).isEqualTo(42 + 6 + 22 + 90 + 22 + 91).isEqualTo(273);
     }
+
+    @Test
+    public void sum_single_cell() {
+        Matrix matrix = new Matrix(new int[][]{
+                {70, 37, 23, 57, 22, 90, 99},
+                {47, 65, 33, 1, 42, 6, 99},
+                {36, 62, 50, 21, 22, 90, 99},
+                {36, 62, 50, 21, 22, 91, 99},
+                {7, 73, 23, 57, 23, 4, 5}
+        });
+
+        long sum = matrix.sum(4, 1, 4, 1);
+
+        assertThat(sum).isEqualTo(42);
+    }
 }
