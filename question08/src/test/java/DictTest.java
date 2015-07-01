ef20faf88cc585993162fd9c76ae049be0459600
David Gageot <david@gageot.net>
2015-07-01 17:33:50 +0200

    Add complex test

--- question08/src/test/java/DictTest.java
+++ question08/src/test/java/DictTest.java
@@ -17,7 +17,10 @@ public class DictTest {
     public void find_permutations() {
         assertThat(dict.permutations("a")).containsOnly("ha", "pa", "ah", "am", "an", "as", "at");
         assertThat(dict.permutations("hello")).containsOnly("hell", "cello", "hallo", "hullo", "hells");
+
         assertThat(dict.permutations("cat")).containsOnly("at", "bat", "eat", "fat", "hat", "mat", "oat", "pat", "rat", "sat", "tat", "vat", "cot", "cut", "cab", "cad", "cam", "can", "cap", "car", "caw", "scat", "chat", "coat", "cant", "cart", "cast", "cats");
+        assertThat(dict.permutations("chat")).containsOnly("chant", "chad", "cheat", "that", "coat", "what", "cat", "char", "chats", "hat", "chap", "chart", "chit");
+        assertThat(dict.permutations("chant")).containsOnly("cant", "chat", "chart", "chants");
     }
 
     @Test
@@ -32,5 +35,12 @@ public class DictTest {
         List<String> path = dict.path("hello", "helli");
 
         assertThat(path).isEmpty();
+    }
+
+    @Test
+    public void shortest_path() {
+        List<String> path = dict.path("cat", "chants");
+
+        assertThat(path).containsExactly("cat", "chat", "chant", "chants");
     }
 }
