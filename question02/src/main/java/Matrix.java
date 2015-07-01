259ad3799ae3873673500c953cea317b04f3dd47
David Gageot <david@gageot.net>
2015-07-01 11:07:01 +0200

    Initial test

---
+++ question02/src/main/java/Matrix.java
@@ -0,0 +1,8 @@
+public class Matrix {
+    public Matrix(int[][] values) {
+    }
+
+    public long sum(int x1, int y1, int x2, int y2) {
+        return -1L;
+    }
+}
---
+++ question02/src/test/java/MatrixTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void sum_area() {
+        Matrix matrix = new Matrix(new int[][]{
+                {70, 37, 23, 57, 22, 90, 99},
+                {47, 65, 33, 1, 42, 6, 99},
+                {36, 62, 50, 21, 22, 90, 99},
+                {36, 62, 50, 21, 22, 91, 99},
+                {7, 73, 23, 57, 23, 4, 5}
+        });
+
+        long sum = matrix.sum(4, 1, 5, 3);
+
+        assertThat(sum).isEqualTo(42 + 6 + 22 + 90 + 22 + 91).isEqualTo(273);
+    }
+}
