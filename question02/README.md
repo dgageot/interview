c3731e724076a455f82af0900895c224b1b268b4
David Gageot <david@gageot.net>
2015-07-01 11:37:15 +0200

    Faster precalc

--- question02/README.md
+++ question02/README.md
@@ -1,11 +1,11 @@
 # Matrix Region Sum
 
-Given a matrix of integers and coordinates of a rectangular region within the matrix, find the sum of numbers falling inside the rectangle.
+Given a NxM matrix of integers and coordinates of a rectangular region within the matrix, find the sum of numbers falling inside the rectangle.
 Our program will be called multiple times with different rectangular regions from the same matrix.
 
 Source: http://www.ardendertat.com/2011/09/20/programming-interview-questions-2-matrix-region-sum/
 
 ## Solution
 
-Brute force: 
-Optimal:
+Brute Force: O(NM)
+Optimal: O(NM) for the precalc. O(1) after precalc
--- question02/src/main/java/Matrix.java
+++ question02/src/main/java/Matrix.java
@@ -5,37 +5,29 @@ public class Matrix {
         this.sums = preCalcSums(values);
     }
 
-    private static long[][] preCalcSums(int[][] values) {
-        long[][] sums = new long[values.length][];
-        for (int y = 0; y < values.length; y++) {
-            sums[y] = new long[values[y].length];
-        }
-
-        for (int y = 0; y < values.length; y++) {
-            for (int x = 0; x < sums[y].length; x++) {
-                sums[y][x] = sum(values, 0, 0, x, y);
-            }
-        }
-
-        return sums;
-    }
-
-    public static long sum(int[][] values, int x1, int y1, int x2, int y2) {
-        long sum = 0L;
-
-        for (int y = y1; y <= y2; y++) {
-            for (int x = x1; x <= x2; x++) {
-                sum += values[y][x];
-            }
-        }
-
-        return sum;
-    }
-
     public long sum(int x1, int y1, int x2, int y2) {
         return sums[y2][x2]
                 - sums[y1 - 1][x2]
                 - sums[y2][x1 - 1]
                 + sums[y1 - 1][x1 - 1];
+    }
+
+    private static long[][] preCalcSums(int[][] values) {
+        long[][] sums = new long[values.length][];
+        for (int y = 0; y < values.length; y++) {
+            sums[y] = new long[values[y].length];
+
+            long current = 0;
+            for (int x = 0; x < sums[y].length; x++) {
+                current += values[y][x];
+
+                sums[y][x] = current;
+                if (y > 0) {
+                    sums[y][x] += sums[y - 1][x];
+                }
+            }
+        }
+
+        return sums;
     }
 }
