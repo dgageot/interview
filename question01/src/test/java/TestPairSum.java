409d016da24844d1d54bcff33d4f9ef2b5c6fcef
David Gageot <david@gageot.net>
2015-07-01 08:36:24 +0200

    Multiple results

--- question01/src/test/java/TestPairSum.java
+++ question01/src/test/java/TestPairSum.java
@@ -6,11 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestPairSum {
     @Test
-    public void test() {
+    public void test_sum() {
         int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         List<int[]> pairs = new PairSum(values).get(3);
 
         assertThat(pairs).containsExactly(new int[]{1, 3});
+    }
+
+    @Test
+    public void test_two_solutions() {
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        List<int[]> pairs = new PairSum(values).get(5);
+
+        assertThat(pairs).containsExactly(new int[]{1, 4}, new int[]{2, 3});
     }
 }
