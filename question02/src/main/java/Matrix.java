fd67ecbb85a716b318ec011085fa34cfaad2a3d0
David Gageot <david@gageot.net>
2015-07-01 11:19:19 +0200

    Introduce algo

--- question02/src/main/java/Matrix.java
+++ question02/src/main/java/Matrix.java
@@ -6,10 +6,17 @@ public class Matrix {
     }
 
     public long sum(int x1, int y1, int x2, int y2) {
+        return precalc_sum(0, 0, x2, y2)
+                - precalc_sum(0, 0, x2, y1 - 1)
+                - precalc_sum(0, 0, x1 - 1, y2)
+                + precalc_sum(0, 0, x1 - 1, y1 - 1);
+    }
+
+    private long precalc_sum(int zerox, int zeroy, int x2, int y2) {
         long sum = 0L;
 
-        for (int y = y1; y <= y2; y++) {
-            for (int x = x1; x <= x2; x++) {
+        for (int y = 0; y <= y2; y++) {
+            for (int x = 0; x <= x2; x++) {
                 sum += values[y][x];
             }
         }
