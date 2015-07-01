6bf26643d3046bfc90be81f175ffcd8bf96615d3
David Gageot <david@gageot.net>
2015-07-01 11:14:27 +0200

    Remove duplication

--- question02/src/test/java/MatrixTest.java
+++ question02/src/test/java/MatrixTest.java
@@ -3,33 +3,25 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixTest {
-    @Test
-    public void sum_area() {
-        Matrix matrix = new Matrix(new int[][]{
-                {70, 37, 23, 57, 22, 90, 99},
-                {47, 65, 33, 1, 42, 6, 99},
-                {36, 62, 50, 21, 22, 90, 99},
-                {36, 62, 50, 21, 22, 91, 99},
-                {7, 73, 23, 57, 23, 4, 5}
-        });
-
-        long sum = matrix.sum(4, 1, 5, 3);
-
-        assertThat(sum).isEqualTo(42 + 6 + 22 + 90 + 22 + 91).isEqualTo(273);
-    }
+    Matrix matrix = new Matrix(new int[][]{
+            {70, 37, 23, 57, 22, 90, 99},
+            {47, 65, 33, 1, 42, 6, 99},
+            {36, 62, 50, 21, 22, 90, 99},
+            {36, 62, 50, 21, 22, 91, 99},
+            {7, 73, 23, 57, 23, 4, 5}
+    });
 
     @Test
     public void sum_single_cell() {
-        Matrix matrix = new Matrix(new int[][]{
-                {70, 37, 23, 57, 22, 90, 99},
-                {47, 65, 33, 1, 42, 6, 99},
-                {36, 62, 50, 21, 22, 90, 99},
-                {36, 62, 50, 21, 22, 91, 99},
-                {7, 73, 23, 57, 23, 4, 5}
-        });
-
         long sum = matrix.sum(4, 1, 4, 1);
 
         assertThat(sum).isEqualTo(42);
+    }
+
+    @Test
+    public void sum_area() {
+        long sum = matrix.sum(4, 1, 5, 3);
+
+        assertThat(sum).isEqualTo(42 + 6 + 22 + 90 + 22 + 91).isEqualTo(273);
     }
 }
