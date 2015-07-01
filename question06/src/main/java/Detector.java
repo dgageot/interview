ed5188a3528c863e254573af95f5027bcaeba17c
David Gageot <david@gageot.net>
2015-07-01 14:11:57 +0200

    Failing test

---
+++ question06/src/main/java/Detector.java
@@ -0,0 +1,5 @@
+public class Detector {
+    public boolean isShuffle(String str1, String str2, String shuffle) {
+        return false;
+    }
+}
---
+++ question06/src/test/java/DetectorTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DetectorTest {
+    @Test
+    public void sample() {
+        Detector detector = new Detector();
+
+        boolean isShuffle = detector.isShuffle("abc", "def", "dabecf");
+
+        assertThat(isShuffle).isTrue();
+    }
+}
