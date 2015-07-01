4f8a1f6035b6bcc74904cee5c0f29c1457ad19af
David Gageot <david@gageot.net>
2015-07-01 14:09:23 +0200

    Question06

---
+++ question06/src/test/java/CodeTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CodeTest {
+    @Test
+    public void test() {
+        assertThat(true).isTrue();
+    }
+}
