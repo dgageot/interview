db110d2bc45e879e23ca1722820f42f38452c1a7
David Gageot <david@gageot.net>
2015-07-01 08:39:34 +0200

    Less specific

--- question01/src/test/java/TestPairSum.java
+++ question01/src/test/java/TestPairSum.java
@@ -11,13 +11,13 @@ public class TestPairSum {
     public void test_sum() {
         List<int[]> pairs = pairSum.get(3);
 
-        assertThat(pairs).containsExactly(new int[]{1, 3});
+        assertThat(pairs).containsOnly(new int[]{1, 3});
     }
 
     @Test
     public void test_two_solutions() {
         List<int[]> pairs = pairSum.get(5);
 
-        assertThat(pairs).containsExactly(new int[]{1, 4}, new int[]{2, 3});
+        assertThat(pairs).containsOnly(new int[]{1, 4}, new int[]{2, 3});
     }
 }
