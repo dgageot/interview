2a10650c135c53ba9d1d485b02bf7f28c79a122e
David Gageot <david@gageot.net>
2015-07-02 18:35:46 +0200

    Permutations

--- question11/src/main/java/Permutations.java
+++ question11/src/main/java/Permutations.java
@@ -1,12 +1,21 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
     public List<String> of(String word) {
-        if ("ab".equals(word)) {
-            return Arrays.asList("ab", "ba");
+        List<String> all = new ArrayList<>();
+        add("", word, all);
+        return all;
+    }
+
+    private void add(String prefix, String word, List<String> all) {
+        if (word.length() == 1) {
+            all.add(prefix + word);
+            return;
         }
 
-        return Arrays.asList(word);
+        for (int i = 0; i < word.length(); i++) {
+            add(prefix + word.charAt(i), word.substring(0, i) + word.substring(i+1), all);
+        }
     }
 }
--- question11/src/test/java/CodeTest.java
+++ question11/src/test/java/CodeTest.java
@@ -18,6 +18,17 @@ public class CodeTest {
     public void two_chars() {
         List<String> results = permutations.of("ab");
 
-        assertThat(results).containsExactly("ab", "ba");
+        assertThat(results)
+            .hasSize(2 * 1)
+            .containsExactly("ab", "ba");
+    }
+
+    @Test
+    public void more_chars() {
+        List<String> results = permutations.of("hello");
+
+        assertThat(results)
+            .hasSize(5 * 4 * 3 * 2 * 1)
+            .containsExactly("hello", "helol", "hello", "helol", "heoll", "heoll", "hlelo", "hleol", "hlleo", "hlloe", "hloel", "hlole", "hlelo", "hleol", "hlleo", "hlloe", "hloel", "hlole", "hoell", "hoell", "holel", "holle", "holel", "holle", "ehllo", "ehlol", "ehllo", "ehlol", "eholl", "eholl", "elhlo", "elhol", "ellho", "elloh", "elohl", "elolh", "elhlo", "elhol", "ellho", "elloh", "elohl", "elolh", "eohll", "eohll", "eolhl", "eollh", "eolhl", "eollh", "lhelo", "lheol", "lhleo", "lhloe", "lhoel", "lhole", "lehlo", "lehol", "lelho", "leloh", "leohl", "leolh", "llheo", "llhoe", "lleho", "lleoh", "llohe", "lloeh", "lohel", "lohle", "loehl", "loelh", "lolhe", "loleh", "lhelo", "lheol", "lhleo", "lhloe", "lhoel", "lhole", "lehlo", "lehol", "lelho", "leloh", "leohl", "leolh", "llheo", "llhoe", "lleho", "lleoh", "llohe", "lloeh", "lohel", "lohle", "loehl", "loelh", "lolhe", "loleh", "ohell", "ohell", "ohlel", "ohlle", "ohlel", "ohlle", "oehll", "oehll", "oelhl", "oellh", "oelhl", "oellh", "olhel", "olhle", "olehl", "olelh", "ollhe", "olleh", "olhel", "olhle", "olehl", "olelh", "ollhe", "olleh");
     }
 }
