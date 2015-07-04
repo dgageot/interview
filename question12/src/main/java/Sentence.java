05720863363f389f0e626a2445ff5e4880057c76
David Gageot <david@gageot.net>
2015-07-04 11:18:11 +0200

    First implementation

--- question12/src/main/java/Sentence.java
+++ question12/src/main/java/Sentence.java
@@ -6,14 +6,36 @@ public class Sentence {
     }
 
     public String reverseWords() {
-        String cleanText = text.trim();
-
         StringBuilder reversed = new StringBuilder();
 
-        for (int i = cleanText.length() - 1; i >= 0; i--) {
-            reversed.append(cleanText.charAt(i));
+        String currentWord = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (c == ' ') {
+                if (currentWord.length() > 0) {
+                    appendReversed(currentWord, reversed);
+                    currentWord = "";
+                }
+            } else {
+                currentWord += c;
+            }
+        }
+
+        if (currentWord.length() > 0) {
+            appendReversed(currentWord, reversed);
         }
 
         return reversed.toString();
+    }
+
+    private void appendReversed(String word, StringBuilder appendTo) {
+        if (appendTo.length() > 0) {
+            appendTo.append(' ');
+        }
+        for (int i = word.length() - 1; i >= 0; i--) {
+            appendTo.append(word.charAt(i));
+        }
     }
 }
