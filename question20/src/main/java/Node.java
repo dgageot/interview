ee7a62698198ed034d999a0839830be2e1dfa7a2
David Gageot <david@gageot.net>
2015-07-08 11:25:39 +0200

    Level Order print

--- question20/src/main/java/Node.java
+++ question20/src/main/java/Node.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Node {
   private int value;
   private final Node left;
@@ -16,13 +19,31 @@ public class Node {
   public String levelOrderPrint() {
     StringBuilder print = new StringBuilder();
 
-    print.append(value).append("\n");
+    Deque<Node> current = new LinkedList<>();
+    current.add(this);
+    Deque<Node> next = new LinkedList<>();
 
-    if (left != null) {
-      print.append(left.value).append(" ");
-    }
-    if (right != null) {
-      print.append(right.value).append("\n");
+    while (!current.isEmpty()) {
+      String level = "";
+      for (Node node : current) {
+        if (!level.isEmpty()) {
+          level += " ";
+        }
+        level += node.value;
+
+        if (node.left != null) {
+          next.add(node.left);
+        }
+        if (node.right != null) {
+          next.add(node.right);
+        }
+      }
+      level += "\n";
+
+      print.append(level);
+
+      current = next;
+      next = new LinkedList<>();
     }
 
     return print.toString();
