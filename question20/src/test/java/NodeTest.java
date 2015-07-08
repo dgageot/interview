d771cde6c95ad51a5b54c4fea0a6ec6a81d5d47d
David Gageot <david@gageot.net>
2015-07-08 11:22:05 +0200

    Failing test

--- question20/src/test/java/NodeTest.java
+++ question20/src/test/java/NodeTest.java
@@ -14,4 +14,17 @@ public class NodeTest {
     assertThat(new Node(1, new Node(2), new Node(3)).levelOrderPrint()).isEqualTo("1\n2 3\n");
     assertThat(new Node(1, new Node(4), new Node(5)).levelOrderPrint()).isEqualTo("1\n4 5\n");
   }
+
+  @Test
+  public void print_level_order() {
+    Node root = new Node(1,
+      new Node(2,
+        new Node(4), null),
+      new Node(3,
+        new Node(5),
+        new Node(6))
+    );
+
+    assertThat(root.levelOrderPrint()).isEqualTo("1\n2 3\n4 5 6\n");
+  }
 }
